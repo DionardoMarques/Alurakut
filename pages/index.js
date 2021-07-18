cf28dd6381449d74379c8fbff22e994c51c65d8f
Dionardo Marques <dionardogianluca@gmail.com>
2021-07-18 19:15:08 -0300

    mudança na rota para o site abrir na vercel

--- pages/index.js
+++ pages/index.js
@@ -251,7 +251,7 @@ React.useEffect(function() {
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context)
   const token = cookies.USER_TOKEN;
-  const { isAuthenticated } = await fetch('http://localhost:3000/api/auth', {
+  const { isAuthenticated } = await fetch('https://alurakut-dionardomarques.vercel.app/api/auth', {
     headers: {
         Authorization: token,
     }
